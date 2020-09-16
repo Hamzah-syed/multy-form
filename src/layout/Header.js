4243066ee3c773542e97503afa58c249199ec09c
Hamzah-syed <50793209+Hamzah-syed@users.noreply.github.com>
2020-09-16 14:25:21 +0500

    beta completed

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span className="ml-3 text-xl">Header</span>
+          <span className="ml-3 text-xl">Formik</span>
         </div>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <Link to="/" className="mr-5 hover:text-white">
